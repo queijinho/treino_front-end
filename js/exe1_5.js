a817691490b50f95f535076c395d0d677749dbcb
Nutelinha <matheus.rlima9@aluno.senai.br>
2022-09-08 11:02:18 -0300

    tomara que vai

---
+++ js/exe1_5.js
@@ -0,0 +1,4 @@
+  const jantar = Number(prompt("Valor do jantar R$"))
+  const garcom = jantar * 0.10
+  const total = jantar + garcom
+  alert("Valor total:" + total)
---
+++ js/exe1_6.js
@@ -0,0 +1,8 @@
+function conversor(minutos) {
+    var state = {horas: 0, minutos: 0};
+    
+    state.horas = Math.floor(minutos/60);
+
+    
+
+}
